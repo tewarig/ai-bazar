a00bbc33afe12fb35c2dcff6211601d926ce60b1
tewarig <gauravtewari111@gmail.com>
2024-02-17 13:39:18 +0530

    feat: initial home page ui

--- src/app/page.js
+++ src/app/page.js
@@ -41,67 +41,39 @@ const trending = [
     title: 'The best of the best',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     tags: ['best', 'top'],
-  },
-  {
-    id: 6,
-    title: 'The best of the best',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    tags: ['best', 'top'],
-  },
-  {
-    id: 7,
-    title: 'The best of the best',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    tags: ['best', 'top'],
-  },
-  {
-    id: 8,
-    title: 'The best of the best',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    tags: ['best', 'top'],
-  },
-  {
-    id: 9,
-    title: 'The best of the best',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    tags: ['best', 'top'],
-  },
-  {
-    id: 10,
-    title: 'The best of the best',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    tags: ['best', 'top'],
-  },  
+  }, 
 ];
 
 export default function Home() {
   return (
-    <div className="flex-col justify-center items-center">
-      <div />
-      <div className="flex w-full max-w-sm items-center space-x-2">
+    <div className="h-screen w-screen flex flex-col justify-center items-center  text-center">
+      <div className="flex w-full items-center space-x-2  sm:max-w-sm sm:p-8 md:max-w-md lg:max-w-lg xl:max-w-xl">
         <Input type="search" placeholder="Search" />
         <Button type="submit"><Search size={24} strokeWidth={2} /></Button>
       </div>
-      <div className="flex w-full max-w-sm items-center space-x-2">
+      <div className="flex w-full max-w-sm items-center justify-center space-x-2">
         <Badge variant="outline">Badge</Badge>
         <Badge variant="outline">Badge</Badge>
         <Badge variant="outline">Badge</Badge>
         <Badge variant="outline">Badge</Badge>
       </div>
-      <div>
-        <Card className="p-4 flex-col justify-center align-center  text-center w-auto"> 
+      <div className="mt-8">
+        <Card className="p-4 flex flex-col justify-center align-center  gap-2 text-center "> 
           
-         <h1 className=""> Trending Today</h1>
-         <div className="flex flex-col  max-h-32 flex-wrap  max-w-30 w-auto"> 
+         <h1 className=" text-pretty   font-semibold"> Trending Today</h1>
+         <div className="flex flex-col  max-h-32 flex-wrap  min-w-40"> 
          {
             trending.map((item,index) => (
-              <div key={item.id}>
+              <div key={item.id} >
                 <span className=""> {index + 1} </span>.
-                <span className=""> {item.title}</span> 
+                <span className=" cursor-pointer hover:underline "> {item.title}</span> 
               </div>
             ))
          }̦
          </div>
+         <div className="flex flex-col  max-h-32 flex-wrap cursor-pointer hover:underline  font-normal">
+          View More
+          </div>
         </Card>
       </div>
 
