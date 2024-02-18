c92f9eb113ce0e529d6516283a323beefdf511b3
tewarig <gauravtewari111@gmail.com>
2024-02-18 13:48:14 +0530

    chore: remove error

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -4,12 +4,10 @@ const getHomeData = async () => {
         const res = await fetch(API.GET_HOME_DATA,{
             next: { revalidate: 1 } 
         });
-        // The return value is *not* serialized
-        // You can return Date, Map, Set, etc.
-       
-        if (!res.ok) {
-          throw new Error('Failed to fetch data')
-        }
+        
+        // if (!res.ok) {
+        //   throw new Error('Failed to fetch data')
+        // }
         return res.json();      
 }
 
