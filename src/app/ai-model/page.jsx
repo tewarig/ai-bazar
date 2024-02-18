cec4a3de3ee95620f81db622e34ef02039c1507c
tewarig <gauravtewari111@gmail.com>
2024-02-19 00:30:43 +0530

    fix: modal value

--- src/app/ai-model/page.jsx
+++ src/app/ai-model/page.jsx
@@ -3,11 +3,11 @@ import { getModels ,getAllMetaData } from "@lib/api";
 import ModelSearch from "@/components/ui/modelSearch";
 
 async function Page() {
-  const { Models } = await getModels();
+  const ModelsData = await getModels();
   const data = await getAllMetaData();
 
   return (
-      <ModelSearch models={Models} categories={data?.categories}   />
+      <ModelSearch models={ModelsData?.Models} categories={data?.categories}   />
   );
 }
 
