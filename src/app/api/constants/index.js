3bdb11a3bdf536099104ca78484421ce7425ef10
tewarig <gauravtewari111@gmail.com>
2024-02-20 04:47:11 +0530

    chore: data update

--- src/app/api/constants/index.js
+++ src/app/api/constants/index.js
@@ -48,6 +48,11 @@ const Models = [
     isFree: false,
     by: "Google",
     description: "Google Gemini is a large-scale deep learning model ",
+    largeDescription:
+      "Google's AI model, emblematic of cutting-edge innovation, epitomizes the pinnacle of artificial intelligence research and development. Rooted in deep learning techniques, Google's AI endeavors encompass a diverse array of applications, ranging from natural language processing and computer vision to reinforcement learning and generative models. At the forefront stands TensorFlow, Google's open-source machine learning framework, empowering developers worldwide to harness the potential of AI for diverse tasks. Moreover, Google's AI ecosystem extends beyond TensorFlow, incorporating sophisticated algorithms and architectures tailored for specific domains, such as the Transformer architecture revolutionizing language processing tasks. Notably, Google's AI prowess manifests in its products and services, from Google Assistant, which leverages advanced natural language understanding, to Google Photos, employing state-of-the-art image recognition. Yet, Google's AI journey is not without ethical considerations, prompting reflections on privacy, bias mitigation, and societal impact. In essence, Google's AI model epitomizes a transformative force, reshaping industries, enhancing user experiences, and ushering humanity into an era of unprecedented technological advancement.    ",
+    aboutCreator:
+      "Google, the omnipresent titan of the digital realm, stands as a modern colossus, shaping the digital landscape in ways unprecedented. Founded in 1998 by Larry Page and Sergey Brin, this Silicon Valley juggernaut swiftly evolved from a mere search engine into a multifaceted conglomerate, offering a plethora of services ranging from email and cloud storage to mobile operating systems and artificial intelligence. Its unassuming interface belies the complex algorithms that power its search engine, which handles billions of queries daily, becoming the go-to gateway for navigating the boundless expanse of the internet. Google's relentless pursuit of innovation extends beyond its core search functionality, delving into ventures such as self-driving cars, quantum computing, and renewable energy. However, its dominance has sparked debates over privacy, data security, and antitrust concerns, underscoring the profound impact it wields over the digital ecosystem. In essence, Google stands not just as a company but as a symbol of the digital age, embodying both its promise and perils.",
+
     example:
       "Code Example - \n - To send a GET request to ``` ai.bazar.vercel.app/getData ``` to get response \n - To send a GET request to ``` ai.bazar.vercel.app/credits ``` to know the balance \n - [view docs](https://www.google.com)    ",
     likes: 200,
@@ -81,7 +86,11 @@ const Models = [
     title: "GPT- 4",
     by: "OpenAI",
     isFree: false,
-    description: "Best in class text generation model",
+    description: "Google Gemini is a large-scale deep learning model ",
+    largeDescription:
+      "GPT-4 represents the cutting edge of AI language models, pushing boundaries further than ever before. With enhanced capabilities in understanding context, nuance, and even emotional cues, GPT-4 fosters more natural and meaningful interactions. Its vast knowledge base, continually updated, ensures up-to-date information and insights across various domains, from science and technology to literature and philosophy. GPT-4 exhibits refined coherence and coherence, producing text that is indistinguishable from human-written content in many cases. Its advanced algorithms enable deeper understanding of complex topics and more accurate responses to queries, making it an invaluable tool for researchers, writers, educators, and businesses alike. Moreover, GPT-4's ability to generate creative and imaginative content opens new possibilities in storytelling, content creation, and problem-solving.",
+    aboutCreator:
+      "OpenAI is an artificial intelligence research laboratory consisting of researchers and engineers who are focused on creating artificial general intelligence (AGI), a form of AI that can understand, learn, and apply its intelligence across a wide range of tasks similar to human intelligence. Here are some key points about OpenAI:\n- **Mission**: OpenAI's mission is to ensure that artificial general intelligence (AGI) benefits all of humanity. They aim to achieve this by conducting research in AI and sharing their findings openly.\n- **Research**: OpenAI conducts cutting-edge research in various areas of artificial intelligence, including natural language processing, reinforcement learning, computer vision, robotics, and more. Their research outputs often include publications in academic journals and conferences, as well as releasing code and models to the public.\n- **Transparency and Collaboration**: OpenAI emphasizes transparency in their research and development process.",
     example:
       "Code Example - \n - To send a GET request to ``` ai.bazar.vercel.app/getData ``` to get response \n - To send a GET request to ``` ai.bazar.vercel.app/credits ``` to know the balance \n - [view docs](https://www.google.com)    ",
     type: "text",
@@ -106,7 +115,7 @@ const Models = [
       },
       {
         id: 15,
-        name: "Liked"
+        name: "Liked",
       },
     ],
   },
@@ -120,9 +129,13 @@ const Models = [
     launchDate: 1676747040,
     type: "text",
     by: "Meta",
-    description: "Free Open Source Model by META , GPT alternative",
+    largeDescription:
+      "LAMMA, the Language Model for Media Analysis developed by Meta, signifies a significant leap forward in AI-driven media comprehension. Built upon a foundation of cutting-edge deep learning techniques, LAMMA excels in parsing and understanding vast amounts of textual and multimedia data with remarkable accuracy and speed. Its sophisticated algorithms enable nuanced analysis of content, including sentiment analysis, topic modeling, and semantic understanding. With LAMMA, Meta aims to revolutionize how we interact with and understand media, providing tools for journalists, researchers, and policymakers to navigate the complex landscape of information more effectively. By identifying trends, biases, and emerging narratives, LAMMA empowers users to make informed decisions and engage with media in a more critical and discerning manner.While LAMMA holds tremendous promise for advancing media analysis and understanding, Meta remains committed to addressing concerns around privacy, transparency, and ethical use, ensuring that this powerful technology serves the greater good.",
+    aboutCreator:
+      "Meta, formerly known as Facebook, has evolved into a pioneering tech conglomerate with a diverse portfolio spanning social media, virtual reality, augmented reality, and AI research. With a mission to bring the world closer together, Meta continues to innovate and shape the digital landscape. At the core of Meta's vision is the development of immersive technologies that redefine human connection and communication. Through platforms like Facebook, Instagram, WhatsApp, and Oculus, Meta facilitates global interactions, fosters communities, and enables expression in various forms. Moreover, Meta's commitment to advancing AI research is evident in projects like LAMMA, which aims to revolutionize media analysis, and its ongoing efforts in natural language processing and computer vision.Despite its strides in technology, Meta faces ongoing scrutiny regarding privacy, misinformation, and antitrust concerns. However, the company remains dedicated to addressing these challenges while pushing the boundaries of innovation to create a more connected and inclusive digital world.",
     example:
       "Code Example - \n - To send a GET request to ``` ai.bazar.vercel.app/getData ``` to get response \n - To send a GET request to ``` ai.bazar.vercel.app/credits ``` to know the balance \n - [view docs](https://www.google.com)    ",
+    description: "Free Open Source Model by META , GPT alternative",
     imageUrl:
       "https://utfs.io/f/b984ef15-822f-4232-9c20-4a89d6707a98-989ilr.png",
     categories: [
@@ -148,7 +161,7 @@ const Models = [
       },
       {
         id: 15,
-        name: "Liked"
+        name: "Liked",
       },
     ],
   },
@@ -164,6 +177,10 @@ const Models = [
     launchDate: 1361214240,
     by: "OpenAI",
     description: "Best in class text generation model",
+    largeDescription:
+      " GPT-3, short for Generative Pre-trained Transformer 3, represents a groundbreaking advancement in artificial intelligence and natural language processing. Developed by OpenAI, GPT-3 is the largest and most powerful language model to date, boasting an impressive 175 billion parameters. This massive neural network enables GPT-3 to understand and generate human-like text across a wide range of topics and writing styles. From answering questions and summarizing text to creative writing and code generation, GPT-3 showcases remarkable versatility and proficiency.  Its ability to generate coherent and contextually relevant responses has led to numerous applications across industries, including customer service, content creation, education, and more. GPT-3 has also sparked conversations about the ethical implications of AI and the potential impact on society.Despite its capabilities, GPT-3 is not without limitations, including occasional inaccuracies and biases inherent in its training data. Nevertheless, it stands as a testament to the rapid progress of AI and its transformative potential in shaping the future of human-machine interaction. ",
+    aboutCreator:
+      "OpenAI is an artificial intelligence research laboratory consisting of researchers and engineers who are focused on creating artificial general intelligence (AGI), a form of AI that can understand, learn, and apply its intelligence across a wide range of tasks similar to human intelligence. Here are some key points about OpenAI:\n- **Mission**: OpenAI's mission is to ensure that artificial general intelligence (AGI) benefits all of humanity. They aim to achieve this by conducting research in AI and sharing their findings openly.\n- **Research**: OpenAI conducts cutting-edge research in various areas of artificial intelligence, including natural language processing, reinforcement learning, computer vision, robotics, and more. Their research outputs often include publications in academic journals and conferences, as well as releasing code and models to the public.\n- **Transparency and Collaboration**: OpenAI emphasizes transparency in their research and development process.",
     example:
       "Code Example - \n - To send a GET request to ``` ai.bazar.vercel.app/getData ``` to get response \n - To send a GET request to ``` ai.bazar.vercel.app/credits ``` to know the balance \n - [view docs](https://www.google.com)    ",
     imageUrl:
@@ -193,6 +210,13 @@ const Models = [
     isFree: false,
     by: "OpenAI",
     description: "Best in class text generation model",
+    largeDescription:
+      " GPT-3.5, short for Generative Pre-trained Transformer 3, represents a groundbreaking advancement in artificial intelligence and natural language processing. Developed by OpenAI, GPT-3.5 is the largest and most powerful language model to date, boasting an impressive 175 billion parameters. This massive neural network enables GPT-3.5 to understand and generate human-like text across a wide range of topics and writing styles. From answering questions and summarizing text to creative writing and code generation, GPT-3.5 showcases remarkable versatility and proficiency.  Its ability to generate coherent and contextually relevant responses has led to numerous applications across industries, including customer service, content creation, education, and more. GPT-3.5 has also sparked conversations about the ethical implications of AI and the potential impact on society.Despite its capabilities, GPT-3.5 is not without limitations, including occasional inaccuracies and biases inherent in its training data. Nevertheless, it stands as a testament to the rapid progress of AI and its transformative potential in shaping the future of human-machine interaction. ",
+    aboutCreator:
+      "OpenAI is an artificial intelligence research laboratory consisting of researchers and engineers who are focused on creating artificial general intelligence (AGI), a form of AI that can understand, learn, and apply its intelligence across a wide range of tasks similar to human intelligence. Here are some key points about OpenAI:\n- **Mission**: OpenAI's mission is to ensure that artificial general intelligence (AGI) benefits all of humanity. They aim to achieve this by conducting research in AI and sharing their findings openly.\n- **Research**: OpenAI conducts cutting-edge research in various areas of artificial intelligence, including natural language processing, reinforcement learning, computer vision, robotics, and more. Their research outputs often include publications in academic journals and conferences, as well as releasing code and models to the public.\n- **Transparency and Collaboration**: OpenAI emphasizes transparency in their research and development process.",
+    example:
+      "Code Example - \n - To send a GET request to ``` ai.bazar.vercel.app/getData ``` to get response \n - To send a GET request to ``` ai.bazar.vercel.app/credits ``` to know the balance \n - [view docs](https://www.google.com)    ",
+
     imageUrl:
       "https://utfs.io/f/693c4e37-130e-4bbf-8c1a-bef2b6c87977-bkva19.png",
     categories: [
@@ -209,6 +233,10 @@ const Models = [
   {
     id: 7,
     title: "Midjourney",
+    largeDescription:
+      " Midjourney is a text-to-image AI tool that lets your imagination take the reins. Picture a sculptor, but instead of chiseling stone, it wields the power of machine learning to craft stunning visuals from your words. Feed it a description A cyberpunk city bathed in neon light, and watch it birth a detailed cityscape buzzing with digital life. It's not just about replicating, it can dream up fantastical landscapes, creatures, and scenes beyond human imagination.    Unlike other tools, Midjourney isnt content with a single image. It presents you with variations, letting you explore different styles and interpretations of your prompt. This iterative process is like a creative conversation, where you refine your words and the AI refines its vision. Its accessible for everyone, from artists exploring new mediums to casual users wanting to visualize their wildest ideas ",
+    aboutCreator:
+      "David Holz, the mastermind behind Midjourney, isn't your typical tech entrepreneur. His journey began with sculpting, a craft that instilled in him a deep appreciation for the power of imagination and its physical manifestation. This passion later translated into co-founding Leap Motion, a company pioneering hand-tracking technology. Holz saw limitations in traditional interfaces and yearned for a more intuitive way to interact with digital worlds. This vision fueled his creation of Midjourney, an AI tool that sculpts images from the language we use. He envisions it not as a replacement for human creativity, but as a powerful collaborator, amplifying our imagination and pushing the boundaries of artistic expression.",
     example:
       "Code Example - \n - To send a GET request to ``` ai.bazar.vercel.app/getData ``` to get response \n - To send a GET request to ``` ai.bazar.vercel.app/credits ``` to know the balance \n - [view docs](https://www.google.com)    ",
 
@@ -237,13 +265,20 @@ const Models = [
       },
       {
         id: 15,
-        name: "Liked"
+        name: "Liked",
       },
     ],
   },
   {
     id: 8,
     title: "Wisper",
+    example:
+      "Code Example - \n - To send a GET request to ``` ai.bazar.vercel.app/getData ``` to get response \n - To send a GET request to ``` ai.bazar.vercel.app/credits ``` to know the balance \n - [view docs](https://www.google.com)    ",
+
+    largeDescription:
+      "ChatGPT Whisper isn't a single AI system, but rather a combination of two powerful tools: ChatGPT, the well-known language model, and Whisper, the speech-to-text AI. Together, they offer a unique way to interact with AI using both your voice and text. Imagine speaking your thoughts and questions to ChatGPT, and receiving its responses back in your preferred voice. With Whisper's speech recognition, ChatGPT can understand your spoken prompts, allowing for natural, hands-free communication.  This combination isn't just about convenience. Whisper's ability to handle accents and translate languages can unlock global communication possibilities. Imagine conversing with ChatGPT in your native tongue, or participating in multilingual discussions without language barriers. ",
+    aboutCreator:
+      "OpenAI is an artificial intelligence research laboratory consisting of researchers and engineers who are focused on creating artificial general intelligence (AGI), a form of AI that can understand, learn, and apply its intelligence across a wide range of tasks similar to human intelligence. Here are some key points about OpenAI:\n- **Mission**: OpenAI's mission is to ensure that artificial general intelligence (AGI) benefits all of humanity. They aim to achieve this by conducting research in AI and sharing their findings openly.\n- **Research**: OpenAI conducts cutting-edge research in various areas of artificial intelligence, including natural language processing, reinforcement learning, computer vision, robotics, and more. Their research outputs often include publications in academic journals and conferences, as well as releasing code and models to the public.\n- **Transparency and Collaboration**: OpenAI emphasizes transparency in their research and development process.",
     example:
       "Code Example - \n - To send a GET request to ``` ai.bazar.vercel.app/getData ``` to get response \n - To send a GET request to ``` ai.bazar.vercel.app/credits ``` to know the balance \n - [view docs](https://www.google.com)    ",
 
@@ -322,12 +357,14 @@ const Categories = [
   {
     id: 12,
     name: "Video",
-  },{
+  },
+  {
     id: 13,
     name: "Trending",
-  },{
+  },
+  {
     id: 15,
-    name: "Liked"
-  }
+    name: "Liked",
+  },
 ];
 export { Models, Categories };
