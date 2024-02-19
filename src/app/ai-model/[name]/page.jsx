c1139ab4d47653c1f258d2d411bb82b9f079916c
tewarig <gauravtewari111@gmail.com>
2024-02-20 00:57:42 +0530

    fix: api changes and try now

--- src/app/ai-model/[name]/page.jsx
+++ src/app/ai-model/[name]/page.jsx
@@ -1,18 +1,13 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import {
-  Copy,
-  Heart,
-  Zap,
-  BookOpenText,
-  DollarSign,
-} from "lucide-react";
+import { Copy, Heart, Zap, BookOpenText, DollarSign } from "lucide-react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Markdown from "react-markdown";
 import { getAiModelData } from "@lib/api";
 import LikeButton from "../comp/LikeButton";
 import CopyButton from "../comp/CopyButton";
+import TryNow from "../comp/TryNow";
 
 export default async function Page({ params }) {
   const aiModelData = await getAiModelData({
@@ -31,9 +26,8 @@ export default async function Page({ params }) {
           {Model.by} &nbsp;/&nbsp;{Model.title}
         </div>
         <div className="font-semibold text-3xl"></div>
-         <CopyButton text={Model.by+ " " + Model.title}/>
+        <CopyButton text={Model.by + " " + Model.title} />
         <LikeButton likeCount={Model.likes} />
-      
       </div>
       <div className="text-md mt-2">{Model.description}</div>
       <div className="flex flex-row  justify-between  items-center w-full mt-4">
@@ -47,15 +41,18 @@ export default async function Page({ params }) {
           ))}
         </div>
         <div className="flex flex-row gap-4">
+          <TryNow modelName={Model.title} modelBy={Model.by} />
+
           <Button size="sm" variant="outline" className="flex gap-2">
             {" "}
-            <Zap /> Try now{" "}
+            <BookOpenText /> Code Resources{" "}
           </Button>
-          <Button size="sm" variant="outline" className="flex gap-2">
-            {" "}
-            <BookOpenText /> Read Docs{" "}
-          </Button>
-          <Button size="sm" variant="outline" className="flex gap-2">
+          <Button
+            size="sm"
+            variant="outline"
+            className="flex gap-2"
+            disabled={Model.isFree}
+          >
             {" "}
             <DollarSign /> Pricing{" "}
           </Button>
